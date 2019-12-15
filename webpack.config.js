7b2ff1d83e50c2331056c4672e8701ef06b8d67d
au.zhurov <alexzhurovbiz@gmail.com>
2019-12-15 14:54:22 +0200

    created base boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const poststylus = require('poststylus');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: "development",
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    },
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    'stylus-loader'
+                ],
+            }
+        ],
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                stylus: {
+                    use: [poststylus(['autoprefixer'])]
+                }
+            }
+        }),
+        new webpack.SourceMapDevToolPlugin({exclude: ['/node_modules/']}),
+    ],
+    devServer: {
+        contentBase: __dirname,
+        publicPath: '/dist/',
+        serveIndex: true,
+        watchContentBase: true,
+        quiet: true,
+        hot: true,
+        port: 9000,
+        open: true,
+        filename: 'bundle.js',
+    }
+};
