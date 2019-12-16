52bd018c492099c74f681f7aba8fd75c1d02e323
au.zhurov <alexzhurovbiz@gmail.com>
2019-12-16 21:22:54 +0200

    added base class for main component

---
+++ src/modules/Spider/index.js
@@ -0,0 +1,30 @@
+export default class Spider {
+
+    /**
+     * Lifecycle methods
+     * @return {void}
+     */
+    beforeMount() {
+    }
+
+    /**
+     * lifecycle methods
+     * @return {void}
+     */
+    afterMount() {
+    }
+
+    /**
+     * lifecycle methods
+     * @return {void}
+     */
+    beforeUpdate() {
+    }
+
+    /**
+     * lifecycle methods
+     * @return {void}
+     */
+    afterUpdate() {
+    }
+}
