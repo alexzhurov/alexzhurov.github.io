0d049287f009dbd5b058e43f1d4ed7f7daa642b3
au.zhurov <alexzhurovbiz@gmail.com>
2019-12-20 22:33:00 +0200

    Removed unused parts in base control

--- src/modules/Control/index.js
+++ src/modules/Control/index.js
@@ -4,21 +4,9 @@ export class Control {
     constructor(rootEl) {
         this.roootEl = rootEl;
         this.setHandler = setHandler.bind(this);
-        this.state = {};
-    }
-
-    get state() {
-        return this._state;
-    }
-
-    set state(val) {
-        this._state = val;
     }
 
     _forceUpdate() {
-        //     let event = new Event('updateApp', {bubbles: true,});
-        //     const el = this.el;
-        //     el && el.dispatchEvent(event);
         if (this.el) {
             removeChilds(this.el);
             this.el.remove();
@@ -40,9 +28,8 @@ export class Control {
 
     createEl() {
         if (!this.render) return;
-        let el = getNodeByString(this.render());
+        const el = getNodeByString(this.render());
         this.el = this.bindEvents(el);
-        // console.log("Control.js => createEl ", this.el);
     }
 
 }
